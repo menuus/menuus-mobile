ad1a47edaed29ce97d1267332b20368f65f9d840
Gabriel Kotecki <gabrielkotecki@gmail.com>
2020-04-29 00:51:58 -0300

    UPDATE: refactoring bottom tabs

--- lib/pages/menu-listing.dart
+++ lib/pages/menu-listing.dart
@@ -8,6 +8,51 @@ class MenuListing extends StatefulWidget {
 }
 
 class _MenuListingState extends State<MenuListing> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    RestaurantListing(),
+    AnimatedContainer(color: Colors.blue, duration: Duration(milliseconds: 300)),
+    AnimatedContainer(color: Colors.red, duration: Duration(milliseconds: 300)),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Title')),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.restaurant), title: Text('Restaurantes')),
+          BottomNavigationBarItem(icon: Icon(Icons.restaurant_menu), title: Text('Pratos')),
+          BottomNavigationBarItem(icon: Icon(Icons.kitchen), title: Text('Teste')),
+        ],
+      ),
+      body: _children[_currentIndex],
+    );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+}
+
+// ----------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------
+
+class RestaurantListing extends StatefulWidget {
+  const RestaurantListing({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _RestaurantListingState createState() => _RestaurantListingState();
+}
+
+class _RestaurantListingState extends State<RestaurantListing> {
   List<Food> initialFoodList = []
     ..add(Food('comidinha', 'restaurante', 'quasi architecto beatae vitae'))
     ..add(Food('comidao', 'lanchonete', 'accusantium doloremque laudantium'))
@@ -17,26 +62,15 @@ class _MenuListingState extends State<MenuListing> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Title')),
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(icon: Icon(Icons.restaurant), title: Text('restaurant')),
-        BottomNavigationBarItem(icon: Icon(Icons.restaurant_menu), title: Text('restaurant_menu')),
-      ]),
-      body: Container(
-        child: Center(
-          child: _buildList(context),
+    return Container(
+      child: Center(
+        child: ListView.builder(
+          itemCount: initialFoodList.length,
+          itemBuilder: (context, int) {
+            return FoodCard(initialFoodList[int]);
+          },
         ),
       ),
-    );
-  }
-
-  ListView _buildList(context) {
-    return ListView.builder(
-      itemCount: initialFoodList.length,
-      itemBuilder: (context, int) {
-        return FoodCard(initialFoodList[int]);
-      },
     );
   }
 }
